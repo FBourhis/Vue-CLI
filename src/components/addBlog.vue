a9a8306c24fa252374689b3856ca44b18db31793
FBourhis <frederic.bourhis@outlook.com>
2020-09-25 14:44:28 +0200

    HTTP Request

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="add-blog">
     <h2>Add a new Blog Post</h2>
-    <form>
+    <form v-if="!submitted">
       <label>Blog Title</label>
       <input type="text" v-model.lazy='blog.title' required/>
       <label>Blog Content:</label>
@@ -20,7 +20,11 @@
       <select v-model="blog.author">
         <option v-for="(author, index) in authors" :key="index">{{ author }}</option>
       </select>
+      <button v-on:click.prevent="post">Add Blog</button>
     </form>
+    <div v-if="submitted">
+      <h3>Thanks for adding your post !</h3>
+    </div>
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: </p>
@@ -52,8 +56,21 @@ export default {
         categories: [],
       },
       authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
+      submitted: false,
     }
   },
+  methods: {
+    post: function() {
+      this.$http.post('https://jsonplaceholder.typicode.com/posts', { // or add our express rest api
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1
+      }).then(function(data){
+        console.log(data);
+        this.submitted = true;
+      });
+    }
+  }
 }
 
 </script>
